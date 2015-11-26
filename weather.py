03294df8b705db619286287849312768ec5cd565
thebean69 <thebean69@gmail.com>
2015-11-25 18:14:07 -0700

    Config file support, error checking
     - Added config file support
     - Config file is expected to be in the same directory where the script is saved.
     - Error checking to catch common config mistakes
     - Output now in correct order when viewed in Zabbix history
     - Location is now a configurable item.

--- weather.py
+++ weather.py
@@ -4,16 +4,53 @@
 # Grab current weather conditions and load them into Zabbix
 # Must run only every 3 min MAX to avoid running into the API limit of 500 free hits per day on weather underground.
 
-import os, sys, urllib2, json, subprocess
+import os, sys, urllib2, json, subprocess, ConfigParser
 from tempfile import mkstemp
 
-# Set these variables
-api_key = 'API_KEY'
-zabbix_server = 'ZABBIX_SERVER'
-zabbix_hostname = 'ZABBIX_HOSTNAME'
+# Find where we are and set where to look for config file based on that.
+path = os.path.abspath(os.path.dirname(sys.argv[0]))
+config_file = path + '/weather.cfg'
+if (os.path.isfile(config_file) == False):
+  print "Cannot find config file: {}".format(config_file)
+  print "Please create it and try again."
+  exit (1)
 
+print "Using config file: {}".format(config_file)
+config = ConfigParser.SafeConfigParser({'zabbix_sender': 'zabbix_sender'})
 
-f = urllib2.urlopen('http://api.wunderground.com/api/{}/conditions/q/CO/Fort_Collins.json'.format(api_key))
+# Read configuration values
+try:
+  config.read(config_file)
+  api_key = config.get('weather', 'api_key')
+  location = config.get('weather', 'location')
+  zabbix_server = config.get('weather', 'zabbix_server')
+  zabbix_hostname = config.get('weather', 'zabbix_hostname')
+  zabbix_sender = config.get('weather', 'zabbix_sender')
+except ConfigParser.NoOptionError as e:
+  print "ERROR: Option '{}' not set in section '{}'".format(e.option, e.section)
+  print "Check config file and try again."
+  exit (1)
+
+# Check for default config values that haven't been changed
+if (api_key == '<API_KEY>'
+or zabbix_server == '<ZABBIX_SERVER>'
+or zabbix_hostname == '<ZABBIX_HOSTNAME>'):
+  print "Default config values found."
+  print "Please change them to actual values and try again."
+  exit (1)
+
+# Print current config values
+print "Read these settings from config file:"
+print "api_key: {}".format(api_key)
+print "location: {}".format(location)
+print "zabbix_server: {}".format(zabbix_server)
+print "zabbix_hostname: {}".format(zabbix_hostname)
+print "zabbix_sender: {}".format(zabbix_sender)
+print "\n"
+
+# Get weather data from weather underground and parse it into variables
+f = urllib2.urlopen('http://api.wunderground.com/api/{}/conditions/q/{}.json'.format(api_key, location))
+#f = open('/etc/zabbix/scripts/Fort_Collins.json','r')
 json_string = f.read()
 f.close()
 parsed_json = json.loads(json_string)
@@ -38,7 +75,7 @@ cw_vism = parsed_json['current_observation']['visibility_mi']
 cw_p1in= parsed_json['current_observation']['precip_1hr_in']
 cw_pdin = parsed_json['current_observation']['precip_today_in']
 
-# File is formatted for zabbix trapper values with no timestamps
+# Save temp file of values formatted for zabbix sender with no timestamps
 fd, temp_file = mkstemp()
 f = open(temp_file, 'w')
 f.write("- cw_city %s\n" % (cw_city))
@@ -61,6 +98,7 @@ f.write("- cw_pdin %s\n" % (cw_pdin))
 f.close()
 os.close(fd)
 
+# Display current conditions in human-readable format
 print "Current Conditions:"
 print "Observed at: {}".format(cw_time)
 print "Location: {}".format(cw_city)
@@ -74,6 +112,10 @@ print "Wind: From the {} ({}°) at {}mph, gusting to {}mph".format(cw_wdir, cw_w
 print "Wind Chill: {}°F ({}°C)".format(cw_wcf, cw_wcc)
 
 print "\nSending values to zabbix..."
-subprocess.call(['zabbix_sender', '-z', zabbix_server, '-s', zabbix_hostname, '-i', temp_file])
 
+# Flush stdout buffer to have output print in the correct order.
+sys.stdout.flush()
+
+# Call zabbix sender to send values to zabbix
+subprocess.call([zabbix_sender, '-z', zabbix_server, '-s', zabbix_hostname, '-i', temp_file])
 os.remove(temp_file)
